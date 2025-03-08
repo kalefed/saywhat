1bc9bdfe60ca5f04a6e93d7b2238480f40a1fe27
kale <18kmkf@queensu.ca>
2025-03-08 18:29:07 -0500

    lil updates

--- server/main.py
+++ server/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from preprocessing import Preprocessing
+import OpenAI
+import os
 
 
 class Sentence(BaseModel):
@@ -17,8 +20,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, etc.)
+    allow_headers=["*"],  # Allows all headers
 )
 
 
@@ -29,4 +32,21 @@ async def root():
 
 @app.put("/translations")
 async def set_sentence(sentence: Sentence):
-    return {"message": f"Received sentence: {sentence.text}"}
+    try:
+        pre = Preprocessing()
+
+        connector = OpenAI.OpenAIConnector(api_key=api_key, model="gpt-4o")
+
+        slang_words = pre.read_csv("all_slangs.csv")
+        processed_text = pre.clean_text(sentence.text, slang_words)
+        prompt = "Message:" + sentence.text + "\n Meaning:" + processed_text
+
+        # Make API call and capture any issues
+        translation = connector.prompt(prompt)
+        print(translation)
+
+        return {"message": f"Received sentence: {sentence.text}"}
+
+    except Exception as e:
+        print(f"Error occurred: {str(e)}")
+        return {"error": "Internal Server Error", "details": str(e)}, 500
